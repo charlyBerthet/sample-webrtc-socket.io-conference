c9216e19b6d2cd461c4a2ea650be4be9ab71d749
charlyberthet <charlyberthet94@gmail.com>
2017-11-12 16:22:54 +0100

    add turn server

--- ui/main.js
+++ ui/main.js
@@ -39,6 +39,16 @@ var configuration = {
             url: 'turn:192.158.29.39:3478?transport=tcp',
             credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             username: '28224511:1379330808'
+        },
+        {
+            url: 'turn:turn.bistri.com:80',
+            credential: 'homeo',
+            username: 'homeo'
+        },
+        {
+            url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            credential: 'webrtc',
+            username: 'webrtc'
         }
     ]
 }; 
