5803ab7b95c7bd49407fbb2e254197830d5d4d43
charlyberthet <charlyberthet94@gmail.com>
2017-11-12 16:18:05 +0100

    add turn server

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var localStream = null;
 
 var configuration = { 
     "iceServers": [
-        {
+        /*{
             'url': 'stun:stun.l.google.com:19302'
         },
         {
@@ -23,7 +23,7 @@ var configuration = {
             'url': 'turn:192.158.29.39:3478?transport=tcp',
             'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
             'username': '28224511:1379330808'
-        }
+        }*/
     ]
 }; 
 var listenerPc = null;
